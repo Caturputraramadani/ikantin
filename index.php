7714588e6df957d900ff5eedb01f0ad250226c77
Catur Putra Ramadani <115392492+Caturputraramadani@users.noreply.github.com>
2023-06-07 11:51:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,8 @@ $jumlah = new jumlah();
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php"><i class="fa fa-home"> </i>Beranda</a>
+                        <a class="nav-link active" aria-current="page" href="index.php"><i class="fa fa-home">
+                            </i>Beranda</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa fa-shopping-cart"></i> Beli</a>
@@ -42,7 +43,8 @@ $jumlah = new jumlah();
 
     <div class="container">
         <div class="jumbotron mt-5 py-4 px-5 bg-primary ">
-            <h1 class="display-7 text-white"> <i class="fa fa-shopping-cart"></i> Selamat Datang di iKantin Wikrama!</h1>
+            <h1 class="display-7 text-white"> <i class="fa fa-shopping-cart"></i> Selamat Datang di iKantin Wikrama!
+            </h1>
         </div>
 
         <div class="card">
@@ -62,24 +64,23 @@ $jumlah = new jumlah();
 
                 <?php
                 if (isset($_POST['check'])) {
-                    $jmlbakso = $_POST['bakso'];
-                    $jmlPc = $_POST['bakwan'];
-                    $jmlKo = $_POST['kopi'];
+                    $jmlBakwan = $_POST['bakwan'];
+                    $jmlBakso = $_POST['bakso'];
+                    $jmlKopi = $_POST['kopi'];
                     $jmlKue = $_POST['kue'];
 
-                    // Deklarasikan objek $jumlah dari kelas yang sesuai
-                    $jumlah = new Jumlah(); // Ganti 'Jumlah' dengan kelas yang sesuai
+                    $jumlah = new jumlah();
 
-                    if ($jmlbakso == null) {
-                        $jumlah->getJumlah(0, $jmlPc);
-                    } elseif ($jmlPc == null) {
-                        $jumlah->getJumlah($jmlbakso, $jmlKo);
-                    } elseif ($jmlKo == null) {
-                        $jumlah->getJumlah($jmlKo, 0);
+                    if ($jmlBakwan == null) {
+                        $jumlah->getJumlah(0, $jmlBakso, $jmlKopi, $jmlKue);
+                    } elseif ($jmlBakso == null) {
+                        $jumlah->getJumlah($jmlBakwan, 0, $jmlKopi, $jmlKue);
+                    } elseif ($jmlKopi == null) {
+                        $jumlah->getJumlah($jmlBakwan, $jmlBakso, 0, $jmlKue);
                     } elseif ($jmlKue == null) {
-                        $jumlah->getJumlah($jmlKue, 0);
+                        $jumlah->getJumlah($jmlBakwan, $jmlBakso, $jmlKopi, 0);
                     } else {
-                        $jumlah->getJumlah($jmlbakso, $jmlPc, $jmlKo, $jmlKue);
+                        $jumlah->getJumlah($jmlBakwan, $jmlBakso, $jmlKopi, $jmlKue);
                     }
 
                     $jumlah->setHarga();
@@ -100,33 +101,34 @@ $jumlah = new jumlah();
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <form class="form-group" action="" method="post">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i></i></span>
-                            <input type="number" class="form-control" name="bakwan" id="bakwan" placeholder="Masukkan Jumlah Bakwan Yang Dibeli" readOnly>
+                    <form action="" method="post" id="form">
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Masukkan Jumlah Croissant yang Dibeli</label>
+                            <input type="number" class="form-control" name="bakwan" id="bakwan" value="0">
                         </div>
-                        <div class="input-group mt-3">
-                            <span class="input-group-addon"><i></i></span>
-                            <input type="number" class="form-control" name="bakso" id="bakso" placeholder="Masukkan Jumlah Bakso Yang Dibeli" readOnly>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Masukkan Jumlah Apple Pie yang Dibeli</label>
+                            <input type="number" class="form-control" name="bakso" id="bakso" value="0">
                         </div>
-                        <div class="input-group mt-3">
-                            <span class="input-group-addon"><i></i></span>
-                            <input type="number" class="form-control" name="kopi" id="kopi" placeholder="Masukkan Jumlah Kopi Yang Dibeli" readOnly>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Masukkan Jumlah Coffe Latte yang Dibeli</label>
+                            <input type="number" class="form-control" name="kopi" id="kopi" value="0">
                         </div>
-                        <div class="input-group mt-3">
-                            <span class="input-group-addon"><i></i></span>
-                            <input type="number" class="form-control" name="kue" id="kue" placeholder="Masukkan Jumlah Kue Yang Dibeli" readOnly>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Masukkan Jumlah Cookies yang Dibeli</label>
+                            <input type="number" class="form-control" name="kue" id="kue" value="0">
                         </div>
 
                         <div class="modal-footer">
-                            <button type="button" id="btnbakso" onclick="Onlybakso()" class="btn btn-primary" style="float:left;">Bakso</button>
-                            <button type="button" id="btnbakwan" onclick="Onlybakwan()" class="btn btn-primary" style="float:left;">Bakwan</button>
-                            <button type="button" id="btnkopi" onclick="Onlykopi()" class="btn btn-primary" style="float:left;">Kopi</button>
-                            <button type="button" id="btnkue" onclick="Onlykue()" class="btn btn-primary" style="float:left;">Kue</button>
-                            <button type="button" id="btnkeduanya" onclick="Keduanya()" class="btn btn-primary" style="float:left;"> Semua</button>
+                            <button type="button" id="btnbakwan" onclick="Onlybakwan()" class="btn btn-primary" style="float:left;">Croissant</button>
+                            <button type="button" id="btnbakso" onclick="Onlybakso()" class="btn btn-primary" style="float:left;">Apple Pie</button>
+                            <button type="button" id="btnkopi" onclick="Onlykopi()" class="btn btn-primary" style="float:left;">Coffe Latte</button>
+                            <button type="button" id="btnkue" onclick="Onlykue()" class="btn btn-primary" style="float:left;">Cookies</button>
+                            <button type="button" id="btnkeduanya" onclick="Semua()" class="btn btn-primary" style="float:left;"> Semua</button>
                             <div class="">
                                 <button type="submit" onclick="exit()" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
-                                <button type="submit" class="btn btn-primary" name="check"><i class="fa fa-check"></i> Cek Total</button>
+                                <button type="submit" class="btn btn-primary" name="check"><i class="fa fa-check"></i>
+                                    Cek Total</button>
                             </div>
                         </div>
                     </form>
@@ -137,64 +139,61 @@ $jumlah = new jumlah();
     </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
 
 </body>
 
 </html>
 
 <script type="text/javascript">
+    function Onlybakwan() {
+        document.getElementById("bakwan").readOnly = false;
+        document.getElementById("bakso").readOnly = true;
+        document.getElementById("kopi").readOnly = true;
+        document.getElementById("kue").readOnly = true;
+        document.getElementById("btnbakwan").disabled = true;
+        document.getElementById("btnbakso").disabled = false;
+        document.getElementById("btnkopi").disabled = false;
+        document.getElementById("btnkue").disabled = false;
+
+    }
+
     function Onlybakso() {
-        document.getElementById("bakso").readOnly = false;
         document.getElementById("bakwan").readOnly = true;
+        document.getElementById("bakso").readOnly = false;
         document.getElementById("kopi").readOnly = true;
         document.getElementById("kue").readOnly = true;
         document.getElementById("btnbakwan").disabled = false;
         document.getElementById("btnbakso").disabled = true;
-        document.getElementById("btnkopi").disabled = true;
-        document.getElementById("btnkue").disabled = true;
-
-    }
-
-    function Onlybakwan() {
-        document.getElementById("bakso").readOnly = true;
-        document.getElementById("kopi").readOnly = true;
-        document.getElementById("kue").readOnly = true;
-        document.getElementById("bakwan").readOnly = false;
-        document.getElementById("btnbakwan").disabled = true;
-        document.getElementById("btnbakso").disabled = false;
-        document.getElementById("btnkopi").disabled = true;
-        document.getElementById("btnkue").disabled = true;
-        document.getElementById("kopi").readOnly = true;
-        document.getElementById("kue").readOnly = true;
+        document.getElementById("btnkopi").disabled = false;
+        document.getElementById("btnkue").disabled = false;
     }
 
     function Onlykopi() {
-        document.getElementById("bakso").readOnly = false;
-        document.getElementById("bakwan").readOnly = false;
+        document.getElementById("bakwan").readOnly = true;
+        document.getElementById("bakso").readOnly = true;
+        document.getElementById("kopi").readOnly = false;
+        document.getElementById("kue").readOnly = true;
         document.getElementById("btnbakwan").disabled = false;
         document.getElementById("btnbakso").disabled = false;
         document.getElementById("btnkopi").disabled = true;
-        document.getElementById("btnkue").disabled = true;
-        document.getElementById("kopi").readOnly = false;
-        document.getElementById("kue").readOnly = true;
-        document.getElementById("btnkopi").disabled = false;
-        document.getElementById("btnkue").disabled = true;
+        document.getElementById("btnkue").disabled = false;
 
     }
 
     function Onlykue() {
-        document.getElementById("bakso").readOnly = true;
         document.getElementById("bakwan").readOnly = true;
-        document.getElementById("btnbakwan").disabled = true;
-        document.getElementById("btnbakso").disabled = true;
-        document.getElementById("btnkopi").disabled = true;
-        document.getElementById("btnkue").disabled = false;
+        document.getElementById("bakso").readOnly = true;
         document.getElementById("kopi").readOnly = true;
         document.getElementById("kue").readOnly = false;
+        document.getElementById("btnbakwan").disabled = false;
+        document.getElementById("btnbakso").disabled = false;
+        document.getElementById("btnkopi").disabled = false;
+        document.getElementById("btnkue").disabled = true;
     }
 
-    function Keduanya() {
+    function Semua() {
         document.getElementById("bakso").readOnly = false;
         document.getElementById("bakwan").readOnly = false;
         document.getElementById("btnbakwan").disabled = false;
